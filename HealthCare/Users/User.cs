694adc0b41a6471f162c42e5a601903a8c468ff7
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-04-07 16:05:00 +0200

    Add class User

---
+++ HealthCare/Users/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Users
+{
+    internal class User
+    {
+        public string Name;
+        public string LastName;
+        public string JMBG;
+        public DateTime BirthDate;
+        public string PhoneNumber;
+        public string Address;
+        public string UserName;
+        public string Password;
+        public enum Gender{ 
+            Male,
+            Female
+        }
+
+        public User(string name, string lastName, string jMBG, DateTime birthDate, string phoneNumber, string address, string userName, string password)
+        {
+            Name = name;
+            LastName = lastName;
+            JMBG = jMBG;
+            BirthDate = birthDate;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            UserName = userName;
+            Password = password;
+        }
+    }
+}
