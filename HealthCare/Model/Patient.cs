11e063e6d6fe13259df4de02826e53428905a27b
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-04-07 17:02:30 +0200

    Add classes Patient and MedicalRecord

---
+++ HealthCare/Model/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using HealthCare.Model;
+
+namespace HealthCare.Users
+{
+    internal class Patient : User
+    {
+        public bool Blocked;
+        public MedicalRecord? MedicalRecord;
+
+        public Patient(string name, string lastName, string jMBG, DateTime birthDate, string phoneNumber, string address, string userName, string password,bool blocked, MedicalRecord? medicalRecord) : base(name, lastName, jMBG, birthDate, phoneNumber, address, userName,password)
+        {
+            Blocked = blocked;
+            MedicalRecord = medicalRecord;
+        }
+    }
+}
