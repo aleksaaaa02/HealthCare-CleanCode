59ffdc09a69231690f648ec3e60c07a190fd9cad
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-04-07 17:15:38 +0200

    Add classes Appointment, Doctor, TimeSlot

---
+++ HealthCare/Model/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using HealthCare.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HealthCare.Model
+{
+    internal class Appointment
+    {
+        public Patient? Patient;
+        public Doctor? Doctor;
+        public TimeSlot? TimeSlot;
+
+    }
+}
---
+++ HealthCare/Model/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using HealthCare.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Model
+{
+    internal class Doctor : User
+    {
+
+
+    }
+}
