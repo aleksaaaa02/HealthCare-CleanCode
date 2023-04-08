19997ed2017d18c086b34f53742684a44c2c2b08
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-04-08 20:24:56 +0200

    Fix RoomType

---
+++ HealthCare/Model/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare
+{
+    enum RoomType
+    {
+        Examinational,
+        Operational,
+        PatientCare,
+        Reception,
+        Warehouse
+    }
+    class Room
+    {
+        public string Name { get; set; }
+        public RoomType Type { get; set; }
+        public Room(string name, RoomType type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
