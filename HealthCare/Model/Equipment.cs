c57019371f64d7fdca6de7a9a117eecd69cdeb74
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-04-08 20:05:29 +0200

    Add Equipment

---
+++ HealthCare/Model/Equipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare
+{
+    class Equipment
+    {
+        public string Name { get; set; }
+        public bool Dynamic { get; set; }
+        public enum Type
+        {
+            Examinational,
+            Operational,
+            RoomFurniture,
+            HallwayFurniture
+        }
+
+        public Equipment(string name, bool dynamic)
+        {
+            Name = name;
+            Dynamic = dynamic;
+        }
+    }
+}
