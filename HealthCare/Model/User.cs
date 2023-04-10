b7c14b8ca8747bcf4d4c4beb50d981b81dff3fbb
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-04-10 14:35:11 +0200

    Minor fixes

--- HealthCare/Model/User.cs
+++ HealthCare/Model/User.cs
@@ -35,9 +35,11 @@ namespace HealthCare
             Password = password;
             Gender = gender;
         }
+
         public User()
         {
         }
+
         public string[] ToCSV()
         {
             string[] csvValues = { Name, LastName, JMBG, BirthDate.ToString(), PhoneNumber, Address, UserName, Password, Gender.ToString() };
