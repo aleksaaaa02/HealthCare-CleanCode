07978a04c914cb6bab8c1185c7030523d66153b3
Milan Arezina <arezinamilann@gmail.com>
2023-04-10 22:55:19 +0200

    Add items to ListView for Appointments

--- HealthCare/View/AppointmentView/AppointmentMainView.xaml.cs
+++ HealthCare/View/AppointmentView/AppointmentMainView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HealthCare.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,26 @@ namespace HealthCare.View.AppointmentView
         public AppointmentMainView()
         {
             InitializeComponent();
+            Doctor doc = new Doctor("Aleksa", "Vukomanovic", "123456789", DateTime.Now, "062173224", "Vuka Karadzica", "aleksa123", "radi", Gender.Male, "Hirurg");
+            string[] bolesti = { "dijabetes", "sizofrenija" };
+            MedicalRecord record = new MedicalRecord(185, 80, bolesti);
+            Patient patient = new Patient("Dimitrije", "Gasic", "234567891", DateTime.Now, "06213214", "Trg Dositeja Obradovica 6", "gasara123", "123123", Gender.Male, false, record);
+            TimeSlot nemamIdeju = new TimeSlot(DateTime.Now, new TimeSpan(0,30,0));
+            Appointment appointment = new Appointment(patient, doc, nemamIdeju, false);
+
+            Doctor doc2 = new Doctor("Jelena", "Karleusa", "123456789", DateTime.Now, "062173224", "Vuka Karadzica", "aleksa123", "radi", Gender.Male, "Hirurg");
+            string[] bolesti2 = { "Insomnia" };
+            MedicalRecord record2 = new MedicalRecord(185, 80, bolesti2);
+            Patient patient2 = new Patient("Marija", "Serifovic", "234567891", DateTime.Now, "06213214", "Trg Dositeja Obradovica 6", "gasara123", "123123", Gender.Male, false, record2);
+            TimeSlot nemamIdeju2 = new TimeSlot(DateTime.Now, new TimeSpan(0, 30, 0));
+            Appointment appointment2 = new Appointment(patient2, doc2, nemamIdeju2, false);
+
+
+
+            List<Appointment> appointments = new List<Appointment>();
+            appointments.Add(appointment);
+            appointments.Add(appointment2);
+            appListView.ItemsSource = new ObservableCollection<Appointment>(appointments);
         }
     }
 }
