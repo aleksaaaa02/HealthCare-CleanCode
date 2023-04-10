d26b32a96b870d4f3ee04a3b3afb02d7e6c25be6
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-04-10 21:38:51 +0200

    Add function to AddButton in NurseMainView

--- HealthCare/View/PatientView/NurseMainView.xaml.cs
+++ HealthCare/View/PatientView/NurseMainView.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,10 +21,12 @@ namespace HealthCare.View.PatientView
     /// </summary>
     public partial class NurseMainView : Window
     {
+        private PatientService patientService;
+
         public NurseMainView()
         {
             InitializeComponent();
-            PatientService patientService = new PatientService("..\\..\\..\\da.csv");
+            patientService = new PatientService("..\\..\\..\\da.csv");
             patientService.Load();
             lvPatients.ItemsSource = patientService.Patients;
         }
@@ -31,6 +34,34 @@ namespace HealthCare.View.PatientView
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Patient patient = new Patient();
+            patient.Name = tbName.Text;
+            patient.LastName = tbLastName.Text;
+            patient.JMBG = tbJMBG.Text;
+            patient.BirthDate = DateTime.Parse(tbBirthDate.Text);
+            patient.PhoneNumber = tbPhoneNumber.Text;
+            patient.Address = tbAddress.Text;
+            if (cbMale.IsChecked is bool Checked && Checked){
+                patient.Gender = Gender.Male;
+            }
+            else patient.Gender = Gender.Female;
+            patient.UserName = tbUsername.Text;
+            patient.Password = tbPassword.Text;
+            if (chbBlocked.IsChecked is bool CheckedBlocked && CheckedBlocked)
+            {
+                patient.Blocked = true;
+            }
+            else patient.Blocked = false;
+            patient.MedicalRecord = new MedicalRecord();
+            patientService.CreateAccount(patient);
+            patientService.Save();
+
+            lvPatients.ItemsSource = patientService.Patients;
+
         }
     }
 }
