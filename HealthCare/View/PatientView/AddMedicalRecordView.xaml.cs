9872f83bb9b1492fd94d5d98221353e8f7b5bf8c
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-04-11 14:03:11 +0200

    Add field validation for medical record

--- HealthCare/View/PatientView/AddMedicalRecordView.xaml.cs
+++ HealthCare/View/PatientView/AddMedicalRecordView.xaml.cs
@@ -43,18 +43,41 @@ namespace HealthCare.View.PatientView
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            MedicalRecord medicalRecord = new MedicalRecord();
-            medicalRecord.Height = float.Parse(tbHeight.Text);
-            medicalRecord.Weight = float.Parse(tbWidth.Text);
-            TextRange textRange = new TextRange(
-                rtbMedicalHistory.Document.ContentStart,
-                rtbMedicalHistory.Document.ContentEnd
-            );
-            medicalRecord.MedicalHistory = textRange.Text.Trim().Split(",");
-            patient.MedicalRecord = medicalRecord;
-            patientService.UpdateAccount(patient);
-            patientService.Save();
-            Close();
+            if (ValidateFields())
+            {
+                MedicalRecord medicalRecord = new MedicalRecord();
+                medicalRecord.Height = float.Parse(tbHeight.Text);
+                medicalRecord.Weight = float.Parse(tbWidth.Text);
+                TextRange textRange = new TextRange(
+                    rtbMedicalHistory.Document.ContentStart,
+                    rtbMedicalHistory.Document.ContentEnd
+                );
+                medicalRecord.MedicalHistory = textRange.Text.Trim().Split(",");
+                patient.MedicalRecord = medicalRecord;
+                patientService.UpdateAccount(patient);
+                patientService.Save();
+                Close();
+            }
+            else
+                ShowErrorMessageBox();
+           
+        }
+
+        public bool ValidateFields()
+        {
+            float parsed;
+            if (float.TryParse(tbHeight.Text, out parsed) && float.TryParse(tbWidth.Text, out parsed))
+                return true;
+            return false;
+        }
+
+        public void ShowErrorMessageBox()
+        {
+            string messageBoxText = "Visina i tezina moraju biti brojevi";
+            string content = "Greska";
+            MessageBoxImage icon = MessageBoxImage.Error;
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBox.Show(messageBoxText, content, button, icon);
         }
     }
 }
