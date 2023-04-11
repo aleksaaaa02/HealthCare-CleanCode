b98911530e3b9a0a12979f477999357bc8856f25
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-04-11 23:03:44 +0200

    Fix saving data in files

--- HealthCare/Service/Schedule.cs
+++ HealthCare/Service/Schedule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthCare.Context;
 using HealthCare.Model;
 using HealthCare.Storage;
 
@@ -11,6 +12,11 @@ namespace HealthCare.Service
     static public class Schedule
     {
         public static List<Appointment> Appointments = new();
+
+        public static int NextId()
+        {
+            return Appointments.Max(s => s.AppointmentID) + 1;
+        }
         public static List<Appointment> GetDoctorAppointments(Doctor Doctor)
         {
             List<Appointment> DoctorAppointments = new List<Appointment>();
@@ -51,12 +57,16 @@ namespace HealthCare.Service
             return PatientAppointments;
         }
 
-        public static void CreateAppointment(Appointment appointment)
+        public static bool CreateAppointment(Appointment appointment)
         {
             if (appointment.Patient.IsAvailable(appointment.TimeSlot) && appointment.Doctor.IsAvailable(appointment.TimeSlot))
             {
+                appointment.AppointmentID = NextId();
                 Appointments.Add(appointment);
+                Save(Global.appointmentPath);
+                return true;
             }
+            return false;
         }
 
         public static void UpdateAppointment(Appointment updatedAppointment)
@@ -75,6 +85,7 @@ namespace HealthCare.Service
                     Appointments.Insert(appointmentIndex, appointment);
                 }
 
+                Save(Global.appointmentPath);
             }
         }
 
