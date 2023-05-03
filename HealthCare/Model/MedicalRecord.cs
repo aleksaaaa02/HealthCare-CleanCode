851c8c7adc082ef467110f7e1d89e600bc52059c
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-05-03 20:54:07 +0200

    Add PastAppointments to MedicalRecord

--- HealthCare/Model/MedicalRecord.cs
+++ HealthCare/Model/MedicalRecord.cs
@@ -1,6 +1,7 @@
 ﻿using HealthCare.Serializer;
 using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,31 @@ namespace HealthCare.Model
         public float Height { get; set; }
         public float Weight { get; set; }
         public string[] MedicalHistory { get; set; }
+        public int[] PastAppointments { get; set; }
         public MedicalRecord(float height, float weight, string[] medicalHistory)
         {
             Height = height;
             Weight = weight;
             MedicalHistory = medicalHistory;
+            PastAppointments = new int[0];
+        }
+        public MedicalRecord(float height, float weight, string[] medicalHistory, int[] pastAppointments)
+        {
+            Height = height;
+            Weight = weight;
+            MedicalHistory = medicalHistory;
+            PastAppointments = pastAppointments;
         }
 
         public MedicalRecord() 
         { 
             MedicalHistory = new string[0];
+            PastAppointments = new int[0];
+        }
+
+        public void addAppointment(int appointmentID)
+        {
+            PastAppointments = PastAppointments.Concat(new int[] { appointmentID }).ToArray();
         }
 
         public override string? ToString()
@@ -32,7 +48,8 @@ namespace HealthCare.Model
         public string[] ToCSV()
         {
             string medicalHistory = string.Join("|",MedicalHistory);
-            string[] csvValues = {Height.ToString(), Weight.ToString(), medicalHistory};
+            string pastAppointments = string.Join("|", PastAppointments);
+            string[] csvValues = {Height.ToString(), Weight.ToString(), medicalHistory,pastAppointments};
             return csvValues;
         }
 
@@ -41,6 +58,10 @@ namespace HealthCare.Model
             Height = float.Parse(values[0]);
             Weight = float.Parse(values[1]);
             MedicalHistory = values[2].Split("|");
+            string[] pastAppointments = values[3].Split("|", StringSplitOptions.RemoveEmptyEntries);
+            PastAppointments = new int[pastAppointments.Length];
+            for (int i = 0; i < pastAppointments.Length; i++)
+                PastAppointments[i] = int.Parse(pastAppointments[i]);
         }
     }
 }
