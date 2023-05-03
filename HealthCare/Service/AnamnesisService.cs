d05e668d440f607f4919459b135876dfaba750ea
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-05-03 21:25:05 +0200

    Add AnamnesisService

---
+++ HealthCare/Service/AnamnesisService.cs
@@ -0,0 +1,49 @@
+﻿using HealthCare.Model;
+using HealthCare.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Service
+{
+    internal class AnamnesisService
+    {
+        public List<Anamnesis> Anamneses = new List<Anamnesis>();
+        private CsvStorage<Anamnesis> csvStorage;
+
+        public AnamnesisService(string filepath)
+        {
+            csvStorage = new CsvStorage<Anamnesis>(filepath);
+        }
+
+        public void Load()
+        {
+            Anamneses = csvStorage.Load();
+        }
+
+        public void Save()
+        {
+            csvStorage.Save(Anamneses);
+        }
+
+        public Anamnesis? GetByID(int ID)
+        {
+            return Anamneses.Find(x => x.ID == ID);
+        }
+
+        public int NextId()
+        {
+            return Anamneses.Max(s => s.ID) + 1;
+        }
+
+        public int AddAnamnesis(Anamnesis anamnesis)
+        {
+            int ID = NextId();
+            anamnesis.ID = ID;
+            Anamneses.Add(anamnesis);
+            return ID;
+        }
+    }
+}
