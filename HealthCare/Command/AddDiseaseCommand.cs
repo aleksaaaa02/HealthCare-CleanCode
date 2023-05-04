a1b25c6d19b81e43dfb01a700e240f4f28d21858
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-05-05 00:33:06 +0200

    Add button functionality for patient search feature

---
+++ HealthCare/Command/AddDiseaseCommand.cs
@@ -0,0 +1,35 @@
+﻿using HealthCare.Model;
+using HealthCare.View.DoctorView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HealthCare.Command
+{
+    public class AddDiseaseCommand : CommandBase
+    {
+        private Patient _selectedPatient;
+        private readonly PatientInforamtionViewModel _viewModel;
+
+        public AddDiseaseCommand(Patient patient, PatientInforamtionViewModel viewModel) 
+        { 
+            _selectedPatient = patient;
+            _viewModel = viewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            string newDisease = _viewModel.Disease;
+
+            if (newDisease == null)
+            {
+                MessageBox.Show("Morate uneti bolest u polje", "Greska", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            _selectedPatient.MedicalRecord.MedicalHistory = _selectedPatient.MedicalRecord.MedicalHistory.Concat(new string[] { newDisease }).ToArray();
+            _viewModel.Update();
+        }
+    }
+}
---
+++ HealthCare/Command/SavePatientChangesCommand.cs
@@ -0,0 +1,24 @@
+﻿using HealthCare.Context;
+using HealthCare.View.DoctorView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Command
+{
+    public class SavePatientChangesCommand : CommandBase
+    {
+        private readonly PatientInforamtionViewModel _viewModel;
+        private Hospital _hospital;
+        public SavePatientChangesCommand(Hospital hospital, PatientInforamtionViewModel viewModel) {
+            _hospital = hospital;
+            _viewModel = viewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            _hospital.PatientService.Save();
+        }
+    }
+}
