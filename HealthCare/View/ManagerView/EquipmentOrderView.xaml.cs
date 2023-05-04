4dfeb4ff4f16f05a54f51842cdb9f837035ee3a5
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-05-04 03:33:13 +0200

    Fix EquipmentOrderView and improve InventoryItemListing

--- HealthCare/View/ManagerView/EquipmentOrderView.xaml.cs
+++ HealthCare/View/ManagerView/EquipmentOrderView.xaml.cs
@@ -21,29 +21,64 @@ namespace HealthCare.View.ManagerView
     /// </summary>
     public partial class EquipmentOrderView : Window
     {
-        private EquipmentOrderViewModel _model;
+        private DynamicEquipmentListingViewModel _model;
         private Window _loginWindow;
         public EquipmentOrderView(Window loginWindow, Hospital hospital)
         {
             InitializeComponent();
             _loginWindow = loginWindow;
-            _model = new EquipmentOrderViewModel(hospital);
+            _model = new DynamicEquipmentListingViewModel(hospital);
             DataContext = _model;
         }
 
         private void Button_Reset(object sender, RoutedEventArgs e)
         {
-
+            _model.LoadAll();
         }
 
         private void Button_Exit(object sender, RoutedEventArgs e)
         {
-
+            _loginWindow.Show();
+            Close();
         }
 
         private void Button_Order(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void ValidateTextBox(object sender, TextChangedEventArgs e)
+        {
+            TextBox? textBox = sender as TextBox;
+            if (textBox == null) return;
+
+            if (textBox.Text != "" && !int.TryParse(textBox.Text, out _))
+            {
+                TextChange textChange = e.Changes.ElementAt(0);
+                int iAddedLength = textChange.AddedLength;
+                int iOffset = textChange.Offset;
+                textBox.Text = textBox.Text.Remove(iOffset, iAddedLength);
+            }
+            else
+            {
+                _highlightRows();
+            }
+        }
+
+        private void _highlightRows()
+        {
+            foreach (var item in lvDynamicEquipment.Items)
+            {
+                var container = (ListViewItem) lvDynamicEquipment.ItemContainerGenerator.ContainerFromItem(item);
+                if (container != null)
+                {
+                    var tb = ViewUtility.FindChild<TextBox>(container, "tbQuantity");
+                    if (tb is TextBox t && t.Text.Trim() != "")
+                        container.Background = Brushes.Yellow;
+                    else
+                        container.Background = Brushes.White;
+                }
+            }
         }
     }
 }
