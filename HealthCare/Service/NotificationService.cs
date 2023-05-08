6d52da07548573d81705da57b7a0639c8cc56cde
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-05-08 15:24:45 +0200

    Add Notification and NotificationService

---
+++ HealthCare/Service/NotificationService.cs
@@ -0,0 +1,14 @@
+﻿using HealthCare.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Service
+{
+    public class NotificationService : NumericService<Notification>
+    {
+        public NotificationService(string filepath) : base(filepath) { }
+    }
+}
