490fa0aaa9e592d7a2f836a0b5365323e8d7e959
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-05-09 10:38:55 +0200

    Update Manager ViewModels

--- HealthCare/ViewModel/ManagerViewModel/DataViewModel/InventoryItemViewModel.cs
+++ HealthCare/ViewModel/ManagerViewModel/DataViewModel/InventoryItemViewModel.cs
@@ -14,11 +14,11 @@ namespace HealthCare.ViewModel.ManagerViewModel
         public readonly Equipment Equipment;
         public readonly Room Room;
         public string EquipmentName => Equipment.Name;
-        public string EquipmentType => ViewUtil.Translate(Equipment.Type);
+        public string EquipmentType => Utility.Translate(Equipment.Type);
         public string RoomName => Room.Name;
-        public string RoomType => ViewUtil.Translate(Room.Type);
+        public string RoomType => Utility.Translate(Room.Type);
         public int Quantity => _item.Quantity;
-        public string IsDynamic => ViewUtil.Translate(Equipment.Dynamic);
+        public string IsDynamic => Utility.Translate(Equipment.Dynamic);
         public InventoryItemViewModel(InventoryItem item, Equipment equipment, Room room)
         {
             _item = item;
