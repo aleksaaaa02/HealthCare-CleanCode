4e372f199b04e70336d1041ebf411df37247d453
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-05-11 11:56:31 +0200

    Add Validation utility class.

---
+++ HealthCare/View/Validation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.View
+{
+    public class Validation
+    {
+        public static bool IsNatural(string number)
+        {
+            return int.TryParse(number, out var n) && n > 0;
+        }
+    }
+}
