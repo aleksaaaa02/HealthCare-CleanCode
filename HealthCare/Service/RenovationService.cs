200010ffdba52be0c3e94224b727336010dacdaf
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-05-21 18:43:17 +0200

    Setup RenovationService

---
+++ HealthCare/Service/RenovationService.cs
@@ -0,0 +1,21 @@
+﻿using HealthCare.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Service
+{
+    public class RenovationService
+    {
+        public RenovationService(Inventory inventory)
+        {
+            // _inventory = inventory;
+        }
+        public void ScheduleRenovation(int roomId, TimeSpan span)
+        {
+            // Schedule.OccupyRoom(new RoomAppointment(roomId, span));
+        }
+    }
+}
