9f283f1f1f47e4a1a24b9d4cc508616aed0d41ce
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-05-22 01:12:41 +0200

    Add View for referral feature

--- HealthCare/View/DoctorView/ReferralView/SpecialistReferralView.xaml.cs
+++ HealthCare/View/DoctorView/ReferralView/SpecialistReferralView.xaml.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HealthCare.Context;
+using HealthCare.Model;
+using HealthCare.ViewModel.DoctorViewModel.Referrals;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HealthCare.View.DoctorView.ReferralView
 {
-    /// <summary>
-    /// Interaction logic for SpecialistReferralView.xaml
-    /// </summary>
     public partial class SpecialistReferralView : Window
     {
-        public SpecialistReferralView()
+        public SpecialistReferralView(Hospital hospital, Patient patient)
         {
             InitializeComponent();
+            DataContext = new SpecialistReferralViewModel(hospital, patient);
         }
     }
 }
--- HealthCare/View/DoctorView/ReferralView/TreatmentReferralView.xaml.cs
+++ HealthCare/View/DoctorView/ReferralView/TreatmentReferralView.xaml.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HealthCare.Context;
+using HealthCare.Model;
+using HealthCare.ViewModel.DoctorViewModel.Referrals;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HealthCare.View.DoctorView.ReferralView
 {
-    /// <summary>
-    /// Interaction logic for TreatmentReferralView.xaml
-    /// </summary>
     public partial class TreatmentReferralView : Window
     {
-        public TreatmentReferralView()
+        public TreatmentReferralView(Hospital hospital, Patient patient)
         {
             InitializeComponent();
+            DataContext = new TreatmentReferralViewModel(hospital, patient);
         }
     }
 }
