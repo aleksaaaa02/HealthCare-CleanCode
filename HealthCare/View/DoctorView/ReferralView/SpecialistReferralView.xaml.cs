a1258a6ed7a27ae66237c4188dfde20a055c31ee
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-05-21 15:11:10 +0200

    Add Referral Views

---
+++ HealthCare/View/DoctorView/ReferralView/SpecialistReferralView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HealthCare.View.DoctorView.ReferralView
+{
+    /// <summary>
+    /// Interaction logic for SpecialistReferralView.xaml
+    /// </summary>
+    public partial class SpecialistReferralView : Window
+    {
+        public SpecialistReferralView()
+        {
+            InitializeComponent();
+        }
+    }
+}
