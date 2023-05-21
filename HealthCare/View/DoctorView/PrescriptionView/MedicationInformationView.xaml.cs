8790eec70cc7e948d22ce928df79860f9381ea16
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-05-21 15:10:41 +0200

    Add Prescription View

---
+++ HealthCare/View/DoctorView/PrescriptionView/MedicationInformationView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HealthCare.View.DoctorView.PrescriptionView
+{
+    /// <summary>
+    /// Interaction logic for MedicationInformationView.xaml
+    /// </summary>
+    public partial class MedicationInformationView : Window
+    {
+        public MedicationInformationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
