d1e419802a7c86057e9c6f440502478b3f9e0f7f
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-05-22 16:35:14 +0200

    Fix binding and imports

--- HealthCare/View/DoctorView/PrescriptionView/MedicationInformationView.xaml.cs
+++ HealthCare/View/DoctorView/PrescriptionView/MedicationInformationView.xaml.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HealthCare.Model;
+using HealthCare.ViewModel.DoctorViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HealthCare.View.DoctorView.PrescriptionView
 {
-    /// <summary>
-    /// Interaction logic for MedicationInformationView.xaml
-    /// </summary>
     public partial class MedicationInformationView : Window
     {
-        public MedicationInformationView()
+        public MedicationInformationView(Medication medication)
         {
             InitializeComponent();
+            DataContext = new MedicationInformationViewModel(medication);
         }
     }
 }
--- HealthCare/View/DoctorView/PrescriptionView/PrescriptionView.xaml.cs
+++ HealthCare/View/DoctorView/PrescriptionView/PrescriptionView.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace HealthCare.View.DoctorView.PrescriptionView
 {
-    /// <summary>
-    /// Interaction logic for PrescriptionView.xaml
-    /// </summary>
     public partial class PrescriptionView : Window
     {
         public PrescriptionView()
