97f739fc28c7340437531f2ba4387bc8b638a4ca
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-06-11 15:09:31 +0200

    I tried

---
+++ HealthCare/ViewModel/NurseViewModel/TreatmantsReferralsMVVM/PatientsTreatmantRefarralsViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using HealthCare.Application;
+using HealthCare.Service;
+using HealthCare.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace HealthCare.ViewModel.NurseViewModel.TreatmantsReferralsMVVM
+{
+    public class PatientsTreatmantRefarralsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public TreatmentService treatmentService => Injector.GetService<TreatmentService>();
+        public DoctorService doctorService => Injector.GetService<DoctorService>();
+        public TherapyService therapyService => Injector.GetService<TherapyService>();
+
+        private TreatmentReferral _treatmentReferral;
+        public PatientsTreatmantRefarralsViewModel(TreatmentReferral referral) {
+            treatmentReferral = referral;
+        }
+
+        public TreatmentReferral treatmentReferral {
+            get => _treatmentReferral;
+            set {
+                _treatmentReferral = value;
+            }
+        }
+
+        private int _id;
+        public int Id {
+            get => _id;
+            set {
+                MessageBox.Show("a");
+                _id = _treatmentReferral.Id;
+            }
+        }
+
+        private int _days;
+        public int Days {
+            get => _days;
+            set {
+                _days = _treatmentReferral.DaysOfTreatment;
+            }
+        }
+
+        private string _doctor;
+        public string Doctor {
+            get => _doctor;
+            set {
+                _doctor = doctorService.Get(_treatmentReferral.DoctorJMBG).Name + " " 
+                     + doctorService.Get(_treatmentReferral.DoctorJMBG).LastName;
+            }
+        }
+
+        private string _therapy;
+        public string Therapy
+        {
+            get => _therapy;
+            set {
+                _therapy = string.Join(",", therapyService.Get(_treatmentReferral.TherapyID).InitialMedication);
+            }
+        }
+
+        private ObservableCollection<TreatmentReferral> _referrals;
+        public ObservableCollection<TreatmentReferral> Refferals {
+            get => _referrals;
+            set {
+                if (_referrals != value) {
+                    _referrals = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ HealthCare/ViewModel/NurseViewModel/TreatmantsReferralsMVVM/TreatmantReferralViewModel.cs
@@ -0,0 +1,48 @@
+﻿using HealthCare.Application;
+using HealthCare.Model;
+using HealthCare.Service;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace HealthCare.ViewModel.NurseViewModel.TreatmantsReferralsMVVM
+{
+    public class TreatmantReferralViewModel : ViewModelBase
+    {
+        private ObservableCollection<PatientsTreatmantRefarralsViewModel> _referrals;
+        private readonly TreatmentReferralService _treatmentReferralService;
+        public ObservableCollection<PatientsTreatmantRefarralsViewModel> Referrals
+        {
+            get => _referrals;
+            set
+            {
+                _referrals = value;
+            }
+        }
+
+        private PatientsTreatmantRefarralsViewModel _selected;
+        public PatientsTreatmantRefarralsViewModel Selected {
+            get => _selected;
+            set {
+                _selected = value;
+            }
+        }
+
+
+        public TreatmantReferralViewModel(){
+            _treatmentReferralService = Injector.GetService<TreatmentReferralService>();
+            loadReferrals();
+        }
+
+        public void loadReferrals() {
+            List<TreatmentReferral> referrals = _treatmentReferralService.GetAll();
+            List<PatientsTreatmantRefarralsViewModel> referralsViewModel = new List<PatientsTreatmantRefarralsViewModel>();
+            foreach (TreatmentReferral referral in referrals) {
+                PatientsTreatmantRefarralsViewModel model = new PatientsTreatmantRefarralsViewModel(referral);
+                referralsViewModel.Add(model);
+            }
+            Referrals = new ObservableCollection<PatientsTreatmantRefarralsViewModel>(referralsViewModel);
+        }
+
+    }
+}
