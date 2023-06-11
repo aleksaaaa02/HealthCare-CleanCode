d4602f378003b3e9434dbfff29d56741636e08ff
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-06-11 17:59:50 +0200

    Add AbsenceRequestView and view models

---
+++ HealthCare/ViewModel/ManagerViewModel/AbsenceRequestListingViewModel.cs
@@ -0,0 +1,57 @@
+﻿using HealthCare.Application;
+using HealthCare.Command;
+using HealthCare.Exceptions;
+using HealthCare.Service;
+using HealthCare.ViewModel.ManagerViewModel.Command;
+using HealthCare.ViewModel.ManagerViewModel.DataViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HealthCare.ViewModel.ManagerViewModel
+{
+    public class AbsenceRequestListingViewModel : ViewModelBase
+    {
+        public ICommand ApproveRequestCommand { get; }
+        public ICommand DeclineRequestCommand { get; }
+        public ICommand ExitCommand { get; }
+        public ObservableCollection<AbsenceRequestViewModel> Items { get; }
+
+        private bool _areApproved = false;
+        public bool AreApproved 
+        {
+            get => _areApproved;
+            set 
+            {
+                _areApproved = value;
+                Load();
+            }
+        }
+
+        public AbsenceRequestViewModel? SelectedRequest { get; set; }
+
+        public AbsenceRequestListingViewModel(Window view) 
+        {
+            Items = new ObservableCollection<AbsenceRequestViewModel>();
+
+            ApproveRequestCommand = new ApproveRequestCommand(this);
+            DeclineRequestCommand = new DeclineRequestCommand(this);
+            ExitCommand = new CancelCommand(view);
+            Load();
+        }
+
+        private void Load()
+        {
+            Items.Clear();
+            Injector.GetService<AbsenceRequestService>()
+                .GetAll().Where(r => r.IsApproved == AreApproved)
+                .Select(r => new AbsenceRequestViewModel(r))
+                .ToList().ForEach(model => Items.Add(model));
+        }
+    }
+}
---
+++ HealthCare/ViewModel/ManagerViewModel/DataViewModel/AbsenceRequestViewModel.cs
@@ -0,0 +1,31 @@
+﻿using HealthCare.Application;
+using HealthCare.Model;
+using HealthCare.Service;
+using HealthCare.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.ViewModel.ManagerViewModel.DataViewModel
+{
+    public class AbsenceRequestViewModel
+    {
+        private AbsenceRequest _request;
+
+        public int Id => _request.Id;
+        public string EmployeeName { get; }
+        public string Reason => _request.Reason;
+        public string Start => ViewUtil.ToString(_request.AbsenceDuration.Start, true);
+        public string End => ViewUtil.ToString(_request.AbsenceDuration.End, true);
+        public string IsApproved => ViewUtil.Translate(_request.IsApproved);
+
+        public AbsenceRequestViewModel(AbsenceRequest request)
+        {
+            _request = request;
+            Doctor doctor = Injector.GetService<DoctorService>().Get(request.RequesterJMBG);
+            EmployeeName = doctor.Name + " " + doctor.LastName;
+        }
+    }
+}
