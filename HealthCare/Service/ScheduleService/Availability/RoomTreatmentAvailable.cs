1f277d60142492a236124c2a24fb49605846857a
Dimitrije Gasic <dimitrije.gasic.02@gmail.com>
2023-06-11 14:35:49 +0200

    Add room availablity checker for treatments

---
+++ HealthCare/Service/ScheduleService/Availability/RoomTreatmentAvailable.cs
@@ -0,0 +1,28 @@
+﻿using HealthCare.Application;
+using HealthCare.Model;
+using HealthCare.Service.RenovationService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Service.ScheduleService.Availability
+{
+    public class RoomTreatmentAvailable : IAvailable<int>
+    {
+        private readonly TreatmentService _treatmentService;
+
+        public RoomTreatmentAvailable()
+        {
+            _treatmentService = Injector.GetService<TreatmentService>();
+        }
+
+        public bool IsAvailable(int key, TimeSlot timeSlot)
+        {
+            return _treatmentService.GetAll()
+                .Where(t => t.RoomId == key)
+                .Count(x => x.TreatmentDuration.Overlaps(timeSlot)) < RoomService.PATIENTCARE_CAPACITY;
+        }
+    }
+}
