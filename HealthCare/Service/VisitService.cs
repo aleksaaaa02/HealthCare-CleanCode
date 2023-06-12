f04aa9d7c780b5172f7650803ac1144fb8b48201
Masa Ivanov <masa.ivano04.10@gmail.com>
2023-06-12 17:11:33 +0200

    This might work

---
+++ HealthCare/Service/VisitService.cs
@@ -0,0 +1,20 @@
+﻿using HealthCare.Application;
+using HealthCare.Model;
+using HealthCare.Repository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HealthCare.Service
+{
+    public class VisitService : NumericService<Visit>
+    {
+        public VisitService(IRepository<Visit> repository) : base(repository)
+        {
+        }
+
+        public List<Visit> getVisits(bool isMorning) {
+            return GetAll().Where(x => x.isMorningVisit() == isMorning).ToList();
+        }
+        
+    }
+}
