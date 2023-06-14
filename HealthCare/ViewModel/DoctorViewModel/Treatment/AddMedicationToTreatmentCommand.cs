13769688b01166fd02fca223f5e64fa41c58b69e
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-06-14 13:23:52 +0200

    Merge bugs fix

--- HealthCare/ViewModel/DoctorViewModel/Treatment/AddMedicationToTreatmentCommand.cs
+++ HealthCare/ViewModel/DoctorViewModel/Treatment/AddMedicationToTreatmentCommand.cs
@@ -3,6 +3,7 @@ using HealthCare.Command;
 using HealthCare.Exceptions;
 using HealthCare.Model;
 using HealthCare.Service;
+using HealthCare.Service.UserService;
 using HealthCare.View;
 using HealthCare.ViewModel.DoctorViewModel.Prescriptions;
 
